4c1b1bfd251c670810fe4be617842176c59a6578
Jonas Hægdahl <jonas.haegd@gmail.com>
2021-12-02 20:37:02 +0100

    Day1 Star1

---
+++ Advent/Day1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021.Advent
+{
+    public static class Day1
+    {
+        public static void Star1()
+        {
+            var measurements = File.ReadAllLines(Directory.GetCurrentDirectory() + @"\Measurements.txt");
+            var numericalMeasurements = Array.ConvertAll(measurements, m => int.Parse(m));
+
+            int larger = 0;
+            int previous = 0;
+
+            for (int i = 1; i < numericalMeasurements.Length; i++)
+            {
+                if (numericalMeasurements[i] > previous)
+                {
+                    larger++;
+                    Console.WriteLine($"{numericalMeasurements[i]} (increase)");
+                }
+                else
+                    Console.WriteLine($"{numericalMeasurements[i]} (decrease)");
+                previous = numericalMeasurements[i];
+            }
+            Console.WriteLine($"The total amount of increases is: {larger}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode2021.Advent;
+using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.Star1();
+        }
+    }
+}
