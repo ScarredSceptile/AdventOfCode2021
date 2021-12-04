d9c8792ffb35d8fbf6d09c83b2871320a1cf46a7
Jonas Hægdahl <jonas.haegd@gmail.com>
2021-12-04 18:27:58 +0100

    Refactor Day4

--- Advent/Day4.cs
+++ Advent/Day4.cs
@@ -8,45 +8,55 @@ namespace AdventOfCode2021.Advent
     {
         public static void Star1()
         {
-            var input = Input.Get("Day4");
-            string all = "";
+            var (Numbers, Cards) = Setup();
 
-            foreach (var line in input)
-                all += line + "\n";
-
-            var bingo = all.Split("\n\n");
-            var numbers = bingo[0];
-            bingo = bingo.Skip(1).ToArray();
-
-            List<BingoCard> cards = new List<BingoCard>();
-            foreach (var card in bingo)
-            {
-                var b = new BingoCard(card);
-                cards.Add(b);
-            }
-
-            var nums = Array.ConvertAll(numbers.Split(","), n => int.Parse(n));
             int lastNum = 0;
-            foreach (var num in nums)
+            foreach (var num in Numbers)
             {
                 lastNum = num;
-                foreach (var card in cards)
+                foreach (var card in Cards)
                 {
                     if (card.num.Contains(num))
                     {
                         card.check[Array.IndexOf(card.num, num)] = true;
                     }
                 }
-                cards.ForEach(card => card.CheckBingo());
-                if (cards.Any(card => card.isBingo)) break;
+                Cards.ForEach(card => card.CheckBingo());
+                if (Cards.Any(card => card.isBingo)) break;
             }
 
-            var bingoCard = cards.Where(card => card.isBingo).FirstOrDefault();
+            var bingoCard = Cards.Where(card => card.isBingo).FirstOrDefault();
             if (bingoCard != null)
                 Console.WriteLine(lastNum * bingoCard.GetSum());
         }
 
         public static void Star2()
+        {
+            var (Numbers, Cards) = Setup();
+
+            int lastNum = 0;
+            foreach (var num in Numbers)
+            {
+                lastNum = num;
+                foreach (var card in Cards)
+                {
+                    if (card.num.Contains(num))
+                    {
+                        card.check[Array.IndexOf(card.num, num)] = true;
+                    }
+                }
+                Cards.ForEach(card => card.CheckBingo());
+                if (Cards.All(card => card.isBingo))
+                    break;
+                Cards.RemoveAll(card => card.isBingo);
+            }
+
+            var bingoCard = Cards.FirstOrDefault();
+            if (bingoCard != null)
+                Console.WriteLine(lastNum * bingoCard.GetSum());
+        }
+
+        private static (int[] Numbers, List<BingoCard> Cards) Setup()
         {
             var input = Input.Get("Day4");
             string all = "";
@@ -55,45 +65,14 @@ namespace AdventOfCode2021.Advent
                 all += line + "\n";
 
             var bingo = all.Split("\n\n");
-            var numbers = bingo[0];
+            var nums = Array.ConvertAll(bingo[0].Split(","), n => int.Parse(n));
             bingo = bingo.Skip(1).ToArray();
 
             List<BingoCard> cards = new List<BingoCard>();
             foreach (var card in bingo)
-            {
-                var b = new BingoCard(card);
-                cards.Add(b);
-            }
+                cards.Add(new BingoCard(card));
 
-            var nums = Array.ConvertAll(numbers.Split(","), n => int.Parse(n));
-            int lastNum = 0;
-            foreach (var num in nums)
-            {
-                lastNum = num;
-                if (cards.Count != 1)
-                {
-                    foreach (var card in cards)
-                    {
-                        if (card.num.Contains(num))
-                        {
-                            card.check[Array.IndexOf(card.num, num)] = true;
-                        }
-                    }
-                    cards.ForEach(card => card.CheckBingo());
-                    cards.RemoveAll(card => card.isBingo);
-                }
-                else
-                {
-                    if (cards[0].num.Contains(num))
-                        cards[0].check[Array.IndexOf(cards[0].num, num)] = true;
-                    cards[0].CheckBingo();
-                    if (cards[0].isBingo) break;
-                }
-            }
-
-            var bingoCard = cards.FirstOrDefault();
-            if (bingoCard != null)
-                Console.WriteLine(lastNum * bingoCard.GetSum());
+            return (nums, cards);
         }
 
         private class BingoCard
@@ -104,13 +83,7 @@ namespace AdventOfCode2021.Advent
 
             public BingoCard(string input)
             {
-                var nums = input.Split("\n");
-                string full = "";
-                foreach (var line in nums)
-                    full += line + " ";
-
-                nums = full.Split(" ");
-                nums = nums.Where(s => s.Length != 0).ToArray();
+                var nums = input.Replace("\n", " ").Split(" ").Where(s => s.Length != 0).ToArray();
                 num = Array.ConvertAll(nums, n => int.Parse(n));
                 check = new bool[num.Length];
             }
